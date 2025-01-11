c03a6466e7d3951578f0f0058bffe6dae0fc9939
DJAlexRus <akobelev18@gmail.com>
2025-01-11 17:30:11 +0500

    Add files via upload

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+//Задание 1
+    val registration = Registration("test@ya.ru")
+    println(registration.getEmail())
+    registration.pass = "ldrk44g"
+    println(registration.pass)
+    println()
+//Задание 2
+    val matrix: Array<Array<Int>> = Array(4){Array(4){(10..90).random()}}
+    print2DArr(matrix)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val matrixTemp =searchEvenNumArr(matrix)
+    println("Четных чисел ${matrixTemp.size}: ${matrixTemp.contentToString()}")
+//Задание 3
+    println()
+    val matrixTwo = create2DArr()
+    print2DArr(matrixTwo)
+//Задание 4
+    println()
+    println("Четные числа: ${searchEvenNumArr(matrixTwo).contentToString()}")
+}
+fun searchEvenNumArr(arr2DNum: Array<Array<Int>>): Array<Int>{
+    var arrEven = Array(0){0}
+    var count = 0
+    for (i in arr2DNum.indices){
+        for (j in arr2DNum[0].indices) {
+            if (arr2DNum[i][j] % 2 == 0) {
+                arrEven += arrayOf(arr2DNum[i][j])
+            }
+        }
+    }
+    return arrEven
+}
+fun print2DArr(arr2DNum: Array<Array<Int>>){
+    for (row in arr2DNum){
+        for (element in row){
+            print("${element}\t")
+        }
+        println()
     }
 }
+fun create2DArr():Array<Array<Int>>{
+    val tempArr: Array<Array<Int>> = Array(4){Array(4){(1..16).random()}}
+    return tempArr
+}
+
--- src/Registration.kt
+++ src/Registration.kt
@@ -1,2 +1,16 @@
-class Registration {
+class Registration(email:String) {
+    internal var pass=""
+    set(value){
+        if((value.length>6)){
+            println("Ваш пароль принят")
+            field = value
+        }else{
+            println("Ошибка. Пароль меньше 6 символов")
+            field=""
+        }
+    }
+
+    private var email=email.uppercase()
+    fun getEmail()=email
 }
+//
